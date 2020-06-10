43a1832a1ab2ad00301c1957c47510a3e02a4896
Steffen Weidenauer <stweidenauer@gmail.com>
2020-06-10 16:28:08 +0200

    Bootstrap Chaos

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello Steffen!</h1>'
+    return render_template('Index.html')
 
 
-@app.route('/user/<name>')
-def user(name):
-    return f'<h1>Hello {name}</h1>'
+@app.route('/user')
+def user():
+    return render_template('User.html')
 
 
 if __name__ == '__main__':
