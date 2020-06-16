10863aacd3097c87ae265a35a57498cdefaf40cb
Steffen <stweidenauer@gmail.com>
2020-06-16 16:05:06 +0000

    Neuer Bootstrap App und look

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = "hard to guess string"
+
+
+class NameForm(FlaskForm):
+    name = StringField("Bitte Namen eingeben:", validators=[DataRequired()])
+    submit = SubmitField('Bestätigen')
 
 
 @app.route('/')
@@ -8,9 +19,14 @@ def index():
     return render_template('Index.html')
 
 
-@app.route('/user')
+@app.route('/user', methods=['GET', 'POST'])
 def user():
-    return render_template('User.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('user.html', form=form, name=name)
 
 
 if __name__ == '__main__':
