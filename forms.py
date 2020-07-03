58368a44cde4f6dee34cf1c90661e9c9226fb8d4
Steffen <stweidenauer@gmail.com>
2020-07-03 11:50:40 +0000

    Register und Login Formulare raw gebastelt

---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, PasswordField
+from wtforms.validators import DataRequired, EqualTo, Email
+
+
+class NameForm(FlaskForm):
+    name = StringField("Bitte Namen eingeben:", validators=[DataRequired()])
+    submit = SubmitField('Bestätigen')
+
+
+class CalcForm(FlaskForm):
+    add1 = IntegerField("Erster Summand:", validators=[DataRequired()])
+    add2 = IntegerField("Zweiter Summand:", validators=[DataRequired()])
+    submit = SubmitField('+')
+
+
+class LogInForm(FlaskForm):
+    email = StringField("Email Adress: ", validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username: ", validators=[DataRequired()])
+    email = StringField("Email Adress: ", validators=[DataRequired(), Email()])
+    # , Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired
+
+from forms import CalcForm, NameForm, LogInForm, RegisterForm
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = "hard to guess string"
-
-
-class NameForm(FlaskForm):
-    name = StringField("Bitte Namen eingeben:", validators=[DataRequired()])
-    submit = SubmitField('Bestätigen')
-
-
-class CalcForm(FlaskForm):
-    add1 = IntegerField("Erster Summand:", validators=[DataRequired()])
-    add2 = IntegerField("Zweiter Summand:", validators=[DataRequired()])
-    submit = SubmitField('+')
-
-
-class SubBtn(FlaskForm):
-    submit = SubmitField('Passiert was')
 
 
 @app.route('/')
@@ -31,16 +15,11 @@ def index():
 
 @app.route('/user', methods=['GET', 'POST'])
 def user():
-    name = None
-    text = ''
     form = NameForm()
-    form2 = SubBtn()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    if form2.validate_on_submit():
-        text = "Ein wichtiger Text"
-    return render_template('user.html', form=form, form2=form2, name=name, text=text)
+        session['name'] = form.name.data
+        return redirect(url_for("user"))
+    return render_template('user.html', form=form, name=session.get('name'))
 
 
 @app.route('/calc', methods=['GET', 'POST'])
@@ -55,6 +34,18 @@ def calc():
         add2 = form.add2.data
         result = add1 + add2
     return render_template('calc.html', form=form, result=result)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LogInForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
