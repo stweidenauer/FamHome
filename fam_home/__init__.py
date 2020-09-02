48a8117402b92706c8547db198f06eede9de8e67
Steffen <stweidenauer@gmail.com>
2020-09-02 17:05:48 +0200

    # 6 End Login working all flash message not

--- fam_home/__init__.py
+++ fam_home/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hard to guess string"
@@ -9,5 +10,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from fam_home import routes
--- fam_home/routes.py
+++ fam_home/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, session, redirect, url_for, flash
 from fam_home import app, db, bcrypt
 from fam_home.forms import CalcForm, NameForm, LogInForm, RegisterForm
 from fam_home.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
@@ -43,15 +44,23 @@ def calc():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LogInForm()
     if form.validate_on_submit():
-        flash('Your account has been created! You are now able to log in', 'success')
-        print("whatever")
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            return redirect(url_for('index'))
+        else:
+            flash('Login unsuccessful. Check Email and PW')
     return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegisterForm()
     if form.validate_on_submit():
         hash_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -61,3 +70,15 @@ def register():
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html')
