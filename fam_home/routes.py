3b89e504225e9f6dd8b34a3598c9007be7550513
Steffen <stweidenauer@gmail.com>
2020-09-21 17:13:00 +0200

    # 8 min 35 Post is updateable

--- fam_home/routes.py
+++ fam_home/routes.py
@@ -2,7 +2,7 @@ import os
 import secrets
 
 from PIL import Image
-from flask import render_template, session, redirect, url_for, flash, request
+from flask import render_template, session, redirect, url_for, flash, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 
 from fam_home import app, db, bcrypt
@@ -124,10 +124,31 @@ def new_post():
         db.session.commit()
         flash('Your Post has been created', 'success')
         return redirect(url_for('index'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
 
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update post',
+                           form=form, legend='Update Post')
