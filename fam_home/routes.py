8904a7ca291c85eb9d57ed5cc07ea592d33e82bb
Steffen <stweidenauer@gmail.com>
2020-09-26 11:55:45 +0200

    # 9 min 19 pagination implemented

--- fam_home/routes.py
+++ fam_home/routes.py
@@ -16,8 +16,8 @@ def index():
     pictures = []
     for picture in os.listdir(path_to_pictures):
         pictures.append(url_for('static', filename='pictures/' + picture))
-    print(pictures)
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('Index.html', pictures=pictures, posts=posts)
 
 
